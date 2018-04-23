8e21ca118cceb1547d6991b19d0d3ccc09c6a93f
Nataliia Zhurbenko <natalie.zhurbenko@gmail.com>
2018-04-24 02:26:46 +0300

    Added arrows toggling

---
+++ client/arrows.js
@@ -0,0 +1,48 @@
+
+export default class ToggleArrows {
+	constructor () {
+		this._arrows = document.querySelectorAll(".arrow");
+		this._headers = document.querySelectorAll(".stats-header");
+	}
+
+	addHandlers() {
+		for(let i=0; i<this._headers.length; i++) {
+			this._headers[i].onclick = (e) => this._toggleHeaders(e);
+			this._arrows[i].onclick = (e) => this._toggleArrow(e);
+		}
+	}
+
+	_toggleHeaders (e) {
+		let active_header = e.target;
+		let active_arrow = active_header.children[0];
+
+		if (active_arrow.style.display == 'inline-block') {
+			this._toggleActiveArrow(active_arrow);
+		} else {
+			for(let i=0; i<this._headers.length; i++) {
+				this._arrows[i].style.display = 'none';
+			}
+
+			active_arrow.style.display = 'inline-block';
+		}
+	}
+
+	_toggleArrow (e) {
+		let active_arrow = e.target;
+		e.stopPropagation()
+
+		if (active_arrow.style.display == 'inline-block') {
+			this._toggleActiveArrow(active_arrow);
+		}
+	}
+
+	_toggleActiveArrow(active_arrow) {
+		if (active_arrow.classList.contains('up')) { 
+			active_arrow.classList.remove('up');
+			active_arrow.classList.add('down');
+		} else {
+			active_arrow.classList.remove('down')
+			active_arrow.classList.add('up');
+		}
+	}
+}
